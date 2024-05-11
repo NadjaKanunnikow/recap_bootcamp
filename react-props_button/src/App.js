cf57288a03265835bb0619bd97e4ad9c82651972
Nadja Kanunnikow <nadja.kanunnikow@gmx.de>
2024-05-11 11:39:38 +0200

    handleClick inside Button component

--- react-props_button/src/App.js
+++ react-props_button/src/App.js
@@ -2,12 +2,43 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <Button color="hotpink" disabled={false} text="click me!" />;
+  return (
+    <>
+      <Button
+        color="hotpink"
+        backgroundColor="black"
+        disabled={false}
+        text="click me!"
+        // onClick={handleClick}
+      />
+      <Button
+        color="white"
+        backgroundColor="hotpink"
+        disabled={false}
+        text="don`t click me!"
+      />
+      <Button
+        color="black"
+        backgroundColor="pink"
+        disabled={false}
+        text="ME! ME! Click MEEEEEE!"
+      />
+    </>
+  );
 }
 
-function Button({ color, isDisabled, text }) {
+function Button({ color, isDisabled, backgroundColor, text }) {
+  function handleClick() {
+    return console.log("Adding a click event!");
+  }
+
   return (
-    <button type="button" disabled={isDisabled} style={{ color: color }}>
+    <button
+      onClick={handleClick}
+      type="button"
+      disabled={isDisabled}
+      style={{ color: color, backgroundColor: backgroundColor }}
+    >
       {text}
     </button>
   );
