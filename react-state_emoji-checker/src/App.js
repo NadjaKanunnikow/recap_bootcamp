fde30871ee537494575c4c4202389540cf0b0309
Nadja Kanunnikow <nadja.kanunnikow@gmx.de>
2024-05-12 15:51:26 +0200

    practice event-handler with bonus challenge

--- react-state_emoji-checker/src/App.js
+++ react-state_emoji-checker/src/App.js
@@ -11,35 +11,77 @@ export default function App() {
   const validCode = "🐡🐠🐋";
 
   return (
-    <div className="container">
-      <div className="button-container">
-        <button type="button" onClick={handleClick}>
-          <span role="img" aria-label="Pufferfish">
-            🐡
-          </span>
-        </button>
-        <button type="button" onClick={handleClick}>
-          <span role="img" aria-label="Whale">
-            🐋
-          </span>
-        </button>
-        <button type="button" onClick={handleClick}>
-          <span role="img" aria-label="Clownfish">
-            🐠
-          </span>
-        </button>
-      </div>
+    <>
+      <div className="container">
+        <div className="button-container">
+          <button type="button" onClick={handleClick}>
+            <span role="img" aria-label="Pufferfish">
+              🐡
+            </span>
+          </button>
+          <button type="button" onClick={handleClick}>
+            <span role="img" aria-label="Whale">
+              🐋
+            </span>
+          </button>
+          <button type="button" onClick={handleClick}>
+            <span role="img" aria-label="Clownfish">
+              🐠
+            </span>
+          </button>
+        </div>
 
+        <button
+          type="button"
+          onClick={() => {
+            setEmoji("");
+          }}
+        >
+          Reset
+        </button>
+        <p>Entered code: {emoji}</p>
+        {emoji === validCode && <p>Valid code!</p>}
+        <ButtonList />
+      </div>
+    </>
+  );
+}
+
+// Bonus challenge >>>>ButtonList<<<<
+
+function ButtonList() {
+  const [text, setText] = useState("");
+  function handleClick(event) {
+    setText(
+      text ? text + " " + event.target.textContent : event.target.textContent
+    );
+  }
+  return (
+    <>
+      <button type="button" onClick={handleClick}>
+        The current limit
+      </button>
+      <button type="button" onClick={handleClick}>
+        is
+      </button>
+      <button type="button" onClick={handleClick}>
+        not
+      </button>
+      <button type="button" onClick={handleClick}>
+        the limit
+      </button>
+      <button type="button" onClick={handleClick}>
+        forever
+      </button>
       <button
         type="button"
         onClick={() => {
-          setEmoji("");
+          setText("");
         }}
       >
         Reset
       </button>
-      <p>Entered code: {emoji}</p>
-      {emoji === validCode && <p>Valid code!</p>}
-    </div>
+      <p>Entered text: {text}</p>
+    </>
   );
 }
