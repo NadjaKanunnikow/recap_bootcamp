0cab77bf38120f5b29dd9c2014e0b6fa1715d654
Nadja Kanunnikow <nadja.kanunnikow@gmx.de>
2024-05-12 09:04:16 +0200

    practice nesting

---
+++ react-nesting_navigation-1/src/App.js
@@ -0,0 +1,32 @@
+import "./styles.css";
+
+import Header from "./components/Header";
+import Navigation from "./components/Navigation";
+import Link from "./components/Link";
+import Logo from "./components/Logo";
+import Avatar from "./components/Avatar";
+
+export default function App() {
+  return (
+    <>
+      <Header>
+        <Link href="#">
+          <Logo />
+        </Link>
+        <Navigation>
+          <Link href="#home">Home</Link>
+          <Link href="#about">About</Link>
+          <Link href="#impressum">Impressum</Link>
+        </Navigation>
+        <button
+          type="button"
+          onClick={() => console.log("I could toggle a profile!")}
+          aria-label="toggle profile"
+        >
+          <Avatar />
+        </button>
+      </Header>
+      <main>content goes here…</main>
+    </>
+  );
+}
